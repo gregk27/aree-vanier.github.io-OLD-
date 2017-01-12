0d84027ac996aae063a6dfa77741a5e8b7b32b0d
Aree-Vanier <gregory@ajayinkingston.com>
2017-01-12 09:09:24 -0500

    Create pointInsidePolygon.py

---
+++ python/pointInsidePolygon.py
@@ -0,0 +1,18 @@
+# determine if a point is inside a given polygon or not
+# from http://www.ariel.com.au/a/python-point-int-poly.html
+# poly is a list of (x,y) pairs.
+def check(x,y,poly):
+    n = len(poly)
+    inside = False
+    p1x,p1y = poly[0]
+    for i in range(n+1):
+        p2x,p2y = poly[i % n]
+        if y > min(p1y,p2y):
+            if y <= max(p1y,p2y):
+                if x <= max(p1x,p2x):
+                    if p1y != p2y:
+                        xinters = (y-p1y)*(p2x-p1x)/(p2y-p1y)+p1x
+                    if p1x == p2x or x <= xinters:
+                        inside = not inside
+        p1x,p1y = p2x,p2y
+    return inside
