b651c213b6a866c0373a15212cbefa9eb86c16b6
Aree-Vanier <gregory@ajayinkingston.com>
2017-01-25 09:08:44 -0500

    Update Tanks.py

--- python/Tanks.py
+++ python/Tanks.py
@@ -38,5 +38,6 @@ if(float(webLauncherVersion)>float(launcherVersion)):
 
 webLauncher.close()
 
+if(not os.path.basename(__file__) == "Tanks.py"): os.rename(os.path.basename(__file__), "Tanks.py")
 
 subprocess.call("C:\Python33\python.exe Resources/Launcher.py")
